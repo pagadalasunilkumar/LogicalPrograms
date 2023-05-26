2e2730699a786c32d10bb73a3aeec0b52d0ac455
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-26 10:05:01 +0530

    file is added

---
+++ CouponNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class CouponNumbers
+    {
+        public static void Main()
+        {
+            int[] couponNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Array of distinct coupon numbers
+
+            int totalRandomNumbersNeeded = GetDistinctCouponCount(couponNumbers);
+
+            Console.WriteLine($"Total random numbers needed to obtain all distinct coupons: {totalRandomNumbersNeeded}");
+        }
+
+        public static int GetDistinctCouponCount(int[] couponNumbers)
+        {
+            Random random = new Random();
+            HashSet<int> distinctCoupons = new HashSet<int>();
+
+            int count = 0;
+
+            while (distinctCoupons.Count < couponNumbers.Length)
+            {
+                int randomNumber = random.Next(1, couponNumbers.Length + 1);
+                count++;
+
+                if (!distinctCoupons.Contains(randomNumber))
+                {
+                    distinctCoupons.Add(randomNumber);
+                }
+            }
+
+            return count;
+        }
+    }
+}
