13833d8048cde4d9540f1cc5d3f87ae37466a0a0
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-26 09:27:02 +0530

    [sunil] the file is added

---
+++ FibanacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibanacciSeries
+    {
+        public static void Main()
+        {
+            int n = 10; // Number of terms in the Fibonacci series
+
+            // First two terms of the series
+            int firstTerm = 0;
+            int secondTerm = 1;
+
+            Console.Write($"{firstTerm} {secondTerm}");
+
+            for (int i = 2; i < n; i++)
+            {
+                int nextTerm = firstTerm + secondTerm;
+                Console.Write($" {nextTerm}");
+
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        public static void GenerateFibonacciSeries(int n)
+        {
+            int firstTerm = 0;
+            int secondTerm = 1;
+
+            Console.Write($"{firstTerm} {secondTerm}");
+
+            for (int i = 2; i < n; i++)
+            {
+                int nextTerm = firstTerm + secondTerm;
+                Console.Write($" {nextTerm}");
+
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+        }
+
+    }
+}
