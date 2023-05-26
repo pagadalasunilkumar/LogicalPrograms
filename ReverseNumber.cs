ffd20573d7c08f06c627b5c8fc4bd4570343f7ed
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-26 10:02:26 +0530

    [sunil] the file is added

---
+++ ReverseNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class ReverseNumber
+    {
+        public static void Main()
+        {
+            int number = 12345; // Number to reverse
+
+            int reversedNumber = ReverseNumber(number);
+
+            Console.WriteLine($"Reversed number: {reversedNumber}");
+        }
+
+        public static int ReverseNumber(int number)
+        {
+            int reverse = 0;
+
+            while (number != 0)
+            {
+                int remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number /= 10;
+            }
+
+            return reverse;
+        }
+    }
+}
