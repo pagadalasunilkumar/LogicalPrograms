0750b5a4d23e75bf5a8919bead044052c84cc880
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-26 09:59:37 +0530

    [sunil] the file added

---
+++ PrimeNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public static void prime()
+        {
+            int number = 17; // Number to check if it's a prime number
+
+            bool isPrime = CheckPrimeNumber(number);
+
+            if (isPrime)
+            {
+                Console.WriteLine($"{number} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a prime number.");
+            }
+        }
+
+        public static bool CheckPrimeNumber(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
